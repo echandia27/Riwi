54e453941be5d0fe19810fb596bd6a0cccb66c23
echandia27 <jp.echandia@gmail.com>
2025-11-05 22:00:37 -0500

    Add files via upload

---
+++ prueba1.py
@@ -0,0 +1,13 @@
+#hacer un codigo que se recorra del 0 a la cantidad que se coloque en la terminal e imprimir si la posision es par o impar
+
+numero = int(input("digite el numero"))
+for i in range (numero+1):
+   
+    if i % 2 == 0:
+        print(f"El número {i} es PAR.")
+    else:
+        print(f"El número {i} es IMPAR.")
+
+#crear un programa que me permita pasar de numero decimal a binario 
+        
+
---
+++ semana1.py
@@ -0,0 +1,158 @@
+'''juguetes = "Juguetes"
+insectos = "Insectos"
+ropa = "Ropa"
+
+print(juguetes)
+
+#booleanos
+verdadero = True
+falso = False'''
+
+numero1= 10
+numero2= 30
+numero3= 7
+numero4= 15
+numero5= 20
+numero6= 100
+numero7= 27
+numero8= 50
+numero9= 22
+numero10= 33
+
+edad= 5+6
+print(edad)
+
+musica= 80+90
+print(musica)
+
+telefono= 50+60
+print(telefono)
+
+coctel=27+22
+print(coctel)
+
+perro= numero1 + numero2
+print(perro)
+
+luna= 5 + 3
+print(luna)
+
+vel= 70+80
+print(vel)
+
+garra= 65+48
+print(garra)
+
+corre= 48+326
+print(corre)
+
+lop=7+8
+print(lop)
+
+fgh=4-1
+print(fgh)
+
+jumm=56-30
+print(jumm)
+
+zxcv=700-2
+print(zxcv)
+
+dlf=7-5
+print(dlf)
+
+edc=9-8
+print(edc)
+
+polk=100-50
+print(polk)
+
+compu=70-20
+print(compu)
+
+sandw=80-79
+print(sandw)
+
+ujm=5-3
+print(ujm)
+
+kio=10-5
+print(kio)
+
+muyr=80*80
+print(muyr)
+
+coser= 90*10
+print(coser)
+
+loser=45*20
+print(loser)
+
+gam= 70*72
+print(gam)
+
+rel=32*23
+print(rel)
+
+hjk=800*7
+print(hjk)
+
+red=70*72
+print(red)
+
+blue=100*100
+print(blue)
+
+rty=152*258
+print(rty)
+
+qwe=500*400
+print(qwe)
+
+toser=50/2
+print(toser)
+
+www= 200/4
+print(www)
+
+hijo= 500/5
+print(hijo)
+
+calculo= 1000/10
+print(calculo)
+
+lok= 80/40
+print(lok)
+
+ger=8/2
+print(ger)
+
+iju=10/2
+print(iju)
+
+plos=40/20
+print(plos)
+
+max=50/2
+print(max)
+
+tuy=4/2
+print(tuy)
+
+#operadores de comparacion
+
+10 == 10 #
+10 != 30 #diferencia
+
+for i in range (1,8):
+    print("los numeros son", i)
+
+contador = 1
+
+while contador <=8:
+    print("numero", contador)
+    contador += 2
+
+    #hacer un codigo que se recorra del 0 a la cantidad que se coloque en la terminal e imprimir si la posision es par o impar
+
+    #crear un programa que me permita pasar de numero decimal a binario 
